4d1ac26abcc3ad6c0906b29a43bde57a703aa925
Yongbin Fan <fanyongbin@outlook.com>
2016-11-11 17:51:44 +0800

    Create Answer001.R

---
+++ 001/Answer001.R
@@ -0,0 +1,26 @@
+# Basic answer use loop
+SumMulti <- function(n,x,y){
+num <- 0
+for (i in 1:n-1){
+  
+  if (i %% x == 0 | i %% y == 0){
+    #print(i)
+    num <- num + i
+  }
+}
+print(num)
+}
+
+# Better answer user Inclusion–exclusion principle 
+# Compute the sum of natural numbers below n
+Sum1toN <- function(n){
+  
+  return(n*(n+1)/2)
+}
+
+# Compute the sum of natural numbers below n that are multiples of x or y. 
+SumMultiples <- function(n,x,y){
+  
+  return(Sum1toN((n-1) %/% x) * x + Sum1toN((n-1) %/% y) * y - Sum1toN((n-1) %/% (x*y)) * (x*y))
+}
+
